e5ff7e173b595aa8ab2b1a03d9460d6ac0fc084c
adnanbezerra <adnanbezerra@proton.me>
2023-10-25 21:12:08 -0300

    add kill counter parser

--- src/parser.js
+++ src/parser.js
@@ -1,18 +1,39 @@
 import fs from 'fs';
 const parsedLog = [];
+const initialGame = {
+  "total_kills": 0,
+  "players": [],
+  "kills": {}
+}
+let currentGame = { ...initialGame };
+let gameNumber = 1;
 
-const interpretLog = (log) => {
+const clearWhiteSpaces = (line) => {
+  const newArray = [];
+  for (const key of line) {
+    if (key !== "") newArray.push(key);
+  }
+  return newArray
+}
 
+const interpretLog = (currentLine) => {
+  const splittedLine = currentLine.split(" ");
+  const line = clearWhiteSpaces(splittedLine);
+
+  if (line.includes("Kill:")) {
+    currentGame.total_kills++;
+  }
 }
 
 const parseLog = () => {
   fs.readFile("./src/log/qgames.log", "utf8", (err, file) => {
     const splittedLines = file.split(/\r?\n|\r|\n/g);
 
-    for (let index = 0; index < splittedLines.length; index++) {
-      interpretLog(splittedLines[index]);  
+    for (let index = 11; index < 97; index++) {
+      interpretLog(splittedLines[index]);
     }
-    
+
+    console.log(parsedLog[0]);
   });
 };
 
